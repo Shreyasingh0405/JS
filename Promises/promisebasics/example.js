7a7997367e68e1ea0621578254262058e27df4c7
Subhamsidharth <ssmahapatra1478@gmail.com>
2025-06-07 23:24:15 +0530

    add examples

--- Promises/promisebasics/example.js
+++ Promises/promisebasics/example.js
@@ -14,5 +14,76 @@
 //     proceedToPayment(orderId) 
 // }  )
 
-const GITHUB_API = 
-fetch()
+// const GITHUB_API = "https://github.com/Shreyasingh0405?tab=overview&from=2025-06-01&to=2025-06-07"
+// const user = fetch(GITHUB_API)
+// console.log(user)
+
+
+
+// const URL = "https://jsonplaceholder.typicode.com/posts/1"
+// const user = fetch(URL)
+// console.log(user)
+// user.then(function(data){
+//     console.log(data)
+// })
+
+
+
+//allsettled returns object
+
+const p1 = new Promise((resolve, reject) => {
+    //setTimeout(() => resolve("P1 Success"), 3000);
+    setTimeout(() => reject("P1 fail"), 3000);
+
+  });
+  
+  const p2 = new Promise((resolve, reject) => {
+    //setTimeout(() => resolve("P2 Success"), 1000);
+    setTimeout(() => reject("P2 Fail"), 1000);
+  });
+  
+  const p3 = new Promise((resolve, reject) => {
+    // setTimeout(() => resolve("P3 Success"), 2000);
+    setTimeout(() => reject("P3 Fail"), 2000);
+  });
+  
+  Promise.race([p1, p2, p3])
+    .then(res => {
+      console.log(res);
+    })
+    .catch(err => {
+      console.error(err)});
+
+      Promise.allSettled([p1, p2, p3])
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.error(err)});
+
+    Promise.all([p1, p2, p3])
+    .then(res => {
+      console.log(res);
+    })
+    .catch(err => {
+      console.error(err)});
+
+  Promise.any([p1, p2, p3])
+    .then(res => {
+      console.log(res);
+    })
+    .catch(err => {
+      console.error(err);
+      console.log(err.errors)
+    });
+  
+
+    //race :- first settled value either fail or success
+
+//aggregate error
+
+
+//     settled (Got the result)
+//     /                    \
+// resolve                  reject
+// (success, fulfilled)   (failure, rejected)
